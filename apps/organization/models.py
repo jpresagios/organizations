ec425b1eaf1e13edb1bf5001e1a2c7f13bf225be
Junior Paz Formoso <junior.presagios@gmail.com>
2021-02-25 12:57:38 -0800

    Added constraints to make email unique

--- apps/organization/models.py
+++ apps/organization/models.py
@@ -2,6 +2,7 @@ from django.db import models
 from django.utils.translation import ugettext_lazy as _
 from django.contrib.auth.models import User
 
+User._meta.get_field('email')._unique = True
 
 class Organization(models.Model):
     name = models.CharField(_("Name"), max_length=100)

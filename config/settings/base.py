ee507472df7eb0a1a70dde21b993b936f6467773
Junior Paz Formoso <junior.presagios@gmail.com>
2021-02-25 13:01:23 -0800

    Register app api

--- config/settings/base.py
+++ config/settings/base.py
@@ -45,6 +45,7 @@ THIRD_PARTY_APPS = [
 # Apps specific for this project go here.
 LOCAL_APPS = [
     'organization.apps.OrganizationConfig',
+    'api.apps.ApiConfig'
 ]
 
 # See: https://docs.djangoproject.com/en/dev/ref/settings/#installed-apps

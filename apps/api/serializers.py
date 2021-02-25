0734e94a05efe871a4bb2fc42874192f00ddbbab
Junior Paz Formoso <junior.presagios@gmail.com>
2021-02-25 13:33:44 -0800

    Added serializer for Group Django model

--- apps/api/serializers.py
+++ apps/api/serializers.py
@@ -1,9 +1,15 @@
 from rest_framework import serializers
 from rest_framework_jwt.settings import api_settings
-from django.contrib.auth.models import User
+from django.contrib.auth.models import User, Group
 from django.utils.translation import ugettext_lazy as _
 from django.shortcuts import get_object_or_404
 from django.contrib.auth import authenticate
+
+
+class GroupSerializer(serializers.ModelSerializer):
+    class Meta:
+        fields = ('name', )
+        model = Group
 
 
 class AuthSerializer(serializers.Serializer):

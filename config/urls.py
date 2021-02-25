dfbcf0a006a83b9534066e1b247d29b419ef398d
Junior Paz Formoso <junior.presagios@gmail.com>
2021-02-25 15:01:12 -0800

    Expose swagger endpoint
    OpenAPI documentation for API

--- config/urls.py
+++ config/urls.py
@@ -18,8 +18,24 @@ from django.urls import path, include
 from django.conf import settings
 from django.views import defaults as default_views
 from django.conf.urls.static import static
+from drf_yasg.views import get_schema_view
+from drf_yasg import openapi
+
+
+schema_view = get_schema_view(
+    openapi.Info(
+        title="Organization API",
+        default_version="v1",
+        description="API for Organizations",
+        terms_of_service="https://www.google.com/policies/terms/",
+        license=openapi.License(name="BSD License"),
+
+    ),
+    public=True,
+)
 
 urlpatterns = [
+    path('swagger/', schema_view.with_ui('swagger', cache_timeout=0), name='schema-swagger-ui'),
     path('admin/', admin.site.urls),
     path('api/', include('apps.api.urls')),
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)

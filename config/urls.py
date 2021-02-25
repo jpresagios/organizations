3325a67266b426e54e84a6ffba20f4b04c06b689
Junior Paz Formoso <junior.presagios@gmail.com>
2021-02-25 03:17:42 -0800

    Register api endpoints in root url config

--- config/urls.py
+++ config/urls.py
@@ -21,6 +21,7 @@ from django.conf.urls.static import static
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('api/', include('apps.api.urls')),
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
 
 if settings.DEBUG:

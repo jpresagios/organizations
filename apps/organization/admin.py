ca6d47cae016ca674fb87a513cd97ddd3d91442e
Junior Paz Formoso <junior.presagios@gmail.com>
2021-02-24 23:41:50 -0800

    Register model Organization in Django Admin Site

--- apps/organization/admin.py
+++ apps/organization/admin.py
@@ -1,3 +1,9 @@
 from django.contrib import admin
 
-# Register your models here.
+from .models import Organization, OrganizationMember
+
+
+@admin.register(Organization)
+class OrganizationAdmin(admin.ModelAdmin):
+    list_display = ('name', 'phone', 'address')
+    search_fields = ['name', 'phone']

c242aa2835496d2bcd4b1d6f297e3e4a6caf44b7
Junior Paz Formoso <junior.presagios@gmail.com>
2021-02-25 12:55:51 -0800

    Added serializer to validate user credentials

---
+++ apps/api/serializers.py
@@ -0,0 +1,33 @@
+from rest_framework import serializers
+from rest_framework_jwt.settings import api_settings
+from django.contrib.auth.models import User
+from django.utils.translation import ugettext_lazy as _
+from django.shortcuts import get_object_or_404
+from django.contrib.auth import authenticate
+
+
+class AuthSerializer(serializers.Serializer):
+    email = serializers.CharField()
+    password = serializers.CharField(write_only=True)
+
+    def validate(self, attrs):
+        email = attrs.get('email')
+        password = attrs.get('password')
+
+        if email and password:
+            user_request = get_object_or_404(
+                User,
+                email=email,
+            )
+            user = authenticate(username=user_request.username, password=password)
+
+            if user:
+                if not user.is_active:
+                    msg = _('User account is disabled.')
+                    raise serializers.ValidationError(msg)
+            else:
+                msg = _('Unable to log in with provided credentials.')
+                raise serializers.ValidationError(msg)
+
+        attrs['user'] = user
+        return attrs

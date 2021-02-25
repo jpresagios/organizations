6cf9b4be9db445050b58f03d4d61c4e30b5fed18
Junior Paz Formoso <junior.presagios@gmail.com>
2021-02-24 23:33:52 -0800

    Added model Organization

--- apps/organization/models.py
+++ apps/organization/models.py
@@ -1,3 +1,12 @@
 from django.db import models
+from django.utils.translation import ugettext_lazy as _
+from django.contrib.auth.models import User
 
-# Create your models here.
+
+class Organization(models.Model):
+    name = models.CharField(_("Name"), max_length=100)
+    phone = models.CharField(_("Phone"), max_length=20)
+    address = models.TextField(_("Address"))
+
+    def __str__(self):
+        return self.name

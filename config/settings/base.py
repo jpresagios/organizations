f1f25954d0c9542509cee53ffa857c3cd04ab5a0
Junior Paz Formoso <junior.presagios@gmail.com>
2021-02-25 14:59:11 -0800

    Register drf_yasg app

--- config/settings/base.py
+++ config/settings/base.py
@@ -39,8 +39,8 @@ DJANGO_APPS = [
 
 THIRD_PARTY_APPS = [
     'rest_framework',
-    'rest_framework.authtoken',
-    'corsheaders'
+    'drf_yasg',
+    'corsheaders'    
 ]
 
 # Apps specific for this project go here.

43bea62f9c6883e00dfe7a9ee09a47efb4c46b20
Junior Paz Formoso <junior.presagios@gmail.com>
2021-02-25 12:57:19 -0800

    Register placement to discover Django Apps

--- config/wsgi.py
+++ config/wsgi.py
@@ -8,8 +8,14 @@ https://docs.djangoproject.com/en/3.1/howto/deployment/wsgi/
 """
 
 import os
-
+import sys
 from django.core.wsgi import get_wsgi_application
+
+# This allows easy placement of apps within the interior
+# apps directory.
+app_path = os.path.dirname(os.path.abspath(__file__)).replace('/config', '')
+sys.path.append(os.path.join(app_path, 'apps'))
+
 
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'config.settings.production')
 

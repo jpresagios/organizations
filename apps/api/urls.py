6eefbea783b9ebcf85979a9d29bb1d96430f8fdc
Junior Paz Formoso <junior.presagios@gmail.com>
2021-02-26 21:20:45 -0800

    Rename from organization view to users

--- apps/api/urls.py
+++ apps/api/urls.py
@@ -1,6 +1,6 @@
 from django.urls import path
 from .views.auth import ObtainAuthToken, GroupList
-from .views.organization import UserByOrganizationList
+from .views.users import UserByOrganizationList
 
 urlpatterns = [
     path('auth/login/', ObtainAuthToken.as_view()),

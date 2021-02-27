5a3814664671cb14390e79745af86b556fe1108c
Junior Paz Formoso <junior.presagios@gmail.com>
2021-02-26 21:54:54 -0800

    Register url for endpoint users/<int:pk>/

--- apps/api/urls.py
+++ apps/api/urls.py
@@ -1,10 +1,11 @@
 from django.urls import path
 from .views.auth import ObtainAuthToken, GroupList
-from .views.users import UserByOrganizationList
+from .views.users import UserByOrganizationList, UserDetail
 
 urlpatterns = [
     path('auth/login/', ObtainAuthToken.as_view()),
     path('auth/groups/', GroupList.as_view()),
 
     path('users/', UserByOrganizationList.as_view()),
+    path('users/<int:pk>/', UserDetail.as_view()),
 ]

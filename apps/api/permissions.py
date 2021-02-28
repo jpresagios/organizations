aa07ee6f08da5e26364d0d16e573cf6e33151685
Junior Paz Formoso <junior.presagios@gmail.com>
2021-02-27 18:44:00 -0800

    Added IsAdministratorOrViewerForGET, IsAdministratorForPatch

--- apps/api/permissions.py
+++ apps/api/permissions.py
@@ -8,3 +8,26 @@ class IsAdministratorOrViewer(permissions.BasePermission):
         is_admin_or_viewer = user.groups.filter(name__in=['Viewer', 'Administrator']).exists()
 
         return is_admin_or_viewer
+
+
+class IsAdministratorOrViewerForGET(permissions.BasePermission):
+    def has_permission(self, request, view):        
+        
+        if request.method == 'GET':
+            user = request.user
+
+            is_admin_or_viewer = user.groups.filter(name__in=['Viewer', 'Administrator']).exists()
+            return is_admin_or_viewer
+            
+        return False
+
+class IsAdministratorForPatch(permissions.BasePermission):
+    def has_permission(self, request, view):        
+
+        if request.method == 'PATCH':
+            user = request.user
+
+            is_admin = user.groups.filter(name__in=['Administrator']).exists()
+            return is_admin
+            
+        return False

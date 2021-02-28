923c09ae883706149da3d7603a3a5f6dd4dd5ba1
Junior Paz Formoso <junior.presagios@gmail.com>
2021-02-27 20:42:09 -0800

    Added tests for GET /api/organization/{id}/users

--- apps/api/tests/comparator_utils.py
+++ apps/api/tests/comparator_utils.py
@@ -1,4 +1,27 @@
-def comparate_organization(org_model, org_dict):
+from organization.models import OrganizationMember, Organization
+
+def comparator_organization(org_model, org_dict):
     return org_model.phone == org_dict['phone'] and \
            org_model.name == org_dict['name'] and \
            org_model.address == org_dict['address']
+
+
+def comparator_organization_members(organization, list_members_response):
+    
+    response_member_pk = [member['pk'] for member in list_members_response]
+    
+    # Every member response belong to members organization
+    for member_response_id in response_member_pk:        
+        is_member = OrganizationMember.objects.filter(organization=organization, pk=member_response_id).count()
+
+        if is_member == 0:
+            return False
+
+    # Every member from organization is in the response members
+    for member in organization.members.all():
+        if member.pk not in response_member_pk:
+            return False
+
+    # Every member response is in Organization and every member from Organization in response
+    # => then response contains exactly every member Organization
+    return True
--- apps/api/tests/tests_organizations_endpoints.py
+++ apps/api/tests/tests_organizations_endpoints.py
@@ -8,7 +8,7 @@ from django.contrib.auth.models import User, Group
 from django.db.models import Q
 from api.fake import generate_all
 from .tokens import get_token
-from .comparator_utils import comparate_organization
+from .comparator_utils import comparator_organization, comparator_organization_members
 
 
 class UserTests(APITestCase):
@@ -40,6 +40,7 @@ class UserTests(APITestCase):
             # The users organizations members len is equal to response
             self.assertEqual(user_organization_members.count(), len(response.data))
 
+
     def organization_detail_group(self, group):
         user = User.objects.filter(groups__name=group).first()
         token = get_token(user)
@@ -54,8 +55,9 @@ class UserTests(APITestCase):
             # Validate status code response
             self.assertEqual(response.status_code, 200)
 
-            org_comp = comparate_organization(organization, response.data)
+            org_comp = comparator_organization(organization, response.data)
             self.assertEqual(org_comp, True)
+
 
     def test_organization_detail_admin(self):
         """
@@ -65,6 +67,7 @@ class UserTests(APITestCase):
 
         self.organization_detail_group('Administrator')
 
+
     def test_organization_detail_viewer(self):
         """
         GET /api/organizations/{id}/
@@ -72,6 +75,7 @@ class UserTests(APITestCase):
         """
 
         self.organization_detail_group('Viewer')
+
 
     def test_organization_detail_user(self):
         """
@@ -90,6 +94,7 @@ class UserTests(APITestCase):
                                        format='json')
 
             self.assertEqual(response.status_code, 403)
+
 
     def test_organization_patch(self):
         """
@@ -111,6 +116,7 @@ class UserTests(APITestCase):
             self.assertEqual(response.status_code, 201)
             self.assertEqual(response.data['name'], patch_name)
 
+
     def test_organization_patch_not_admin(self):
         """
         PATCH /api/organizations/{id} Update organization if request user is not `Administrator`
@@ -129,4 +135,60 @@ class UserTests(APITestCase):
                                          HTTP_AUTHORIZATION=f"JWT {token}",
                                          format='json')
 
+            self.assertEqual(response.status_code, 403)
+
+    def organization_members_group(self, group):
+        user = User.objects.filter(groups__name=group).first()
+        token = get_token(user)
+
+        for organization in Organization.objects.all():
+            url_organization_members = reverse('organization_members', args=[organization.pk])
+
+            
+            response = self.client.get(url_organization_members,
+                                         HTTP_AUTHORIZATION=f"JWT {token}",
+                                         format='json')
+
+            self.assertEqual(response.status_code, 200)
+
+            org_members_eq = comparator_organization_members(organization, response.data)
+
+            self.assertEqual(org_members_eq, True)
+
+
+    def test_organization_members_admin(self):
+        """
+        GET /api/organization/{id}/users List all the users for the user organization if user is
+        `Administrator`
+        """
+
+        self.organization_members_group('Administrator')
+
+
+    def test_organization_members_viewer(self):
+        """
+        GET /api/organization/{id}/users List all the users for the user organization if user is
+        `Viewer`
+        """
+
+        self.organization_members_group('Viewer')
+
+
+    def test_organization_members_not_admin_or_viewer(self):
+        """
+        GET /api/organization/{id}/users List all the users for the user organization if user is
+        `Viewer`
+        """
+
+        user = User.objects.filter(~Q(groups__name='Administrator') & ~Q(groups__name='Viewer')).first()
+        token = get_token(user)
+
+        for organization in Organization.objects.all():
+            url_organization_members = reverse('organization_members', args=[organization.pk])
+
+            
+            response = self.client.get(url_organization_members,
+                                         HTTP_AUTHORIZATION=f"JWT {token}",
+                                         format='json')
+            
             self.assertEqual(response.status_code, 403)

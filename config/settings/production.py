28352f8a76196323a6d1fcb503cf015ad78c8f51
Junior Paz Formoso <junior.presagios@gmail.com>
2021-02-28 16:15:13 -0800

    Added WhiteNoise Config in production

--- config/settings/production.py
+++ config/settings/production.py
@@ -1,8 +1,7 @@
 """
 Production Configurations
 
-Added in this files confi prod like Redis for cache
-and other Prod settings
+- Use WhiteNoise for serving static files
 """
 
 from .base import *  # noqa
@@ -13,6 +12,13 @@ DEBUG = env.bool('DJANGO_DEBUG', default=False)
 
 INTERNAL_IPS = ('127.0.0.1',)
 
+# This ensures that Django will be able to detect a secure connection
+# properly on Heroku.
+SECURE_PROXY_SSL_HEADER = ('HTTP_X_FORWARDED_PROTO', 'https')
+# Use Whitenoise to serve static files
+# See: https://whitenoise.readthedocs.io/
+WHITENOISE_MIDDLEWARE = ['whitenoise.middleware.WhiteNoiseMiddleware', ]
+MIDDLEWARE = WHITENOISE_MIDDLEWARE + MIDDLEWARE
 
 # SECRET CONFIGURATION
 # ------------------------------------------------------------------------------
@@ -28,3 +34,8 @@ INSTALLED_APPS += ['gunicorn', ]
 # See https://docs.djangoproject.com/en/dev/ref/settings/#allowed-hosts
 ALLOWED_HOSTS = env.list('DJANGO_ALLOWED_HOSTS', default=['*', ])
 # END SITE CONFIGURATION
+
+
+# Static Assets
+# ------------------------
+STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
